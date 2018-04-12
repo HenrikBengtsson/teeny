7e6ab683d1c67295690d700acafe768f316d3f67
hb <hb@aroma-project.org>
2018-04-12 10:42:50 -0700

    debugme: Adding minimal example using debugme's DEBUG string declarations

--- R/hello.R
+++ R/hello.R
@@ -1,2 +1,11 @@
-hello <- function() message("Hello world!")
+hello <- function() {
+  "!DEBUG hello() ..."
+  "!DEBUG  A random number in [0,100]: `sample.int(100L, size = 1L)`"
+  
+  message("Hello world!")
+  
+  "!DEBUG hello() ... OK"
 
+  ## Avoid returning that last 'debugme' string
+  invisible()
+}
