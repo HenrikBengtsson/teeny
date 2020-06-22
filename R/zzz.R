ce9b2cf9c2eabfaea12842283cd992fd95829ac1
Henrik Bengtsson <hb@aroma-project.org>
2020-06-21 18:57:47 -0700

    stop(paste0(...)) -> stop(...)

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname) {
    if (nzchar(Sys.getenv("R_CMD"))) return()
-   stop(paste0("Package ", sQuote(pkgname), " must not be attached"))
+   stop("Package ", sQuote(pkgname), " must not be attached")
 }
