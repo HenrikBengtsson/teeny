75f418a436f226f7c0fb6e1bce1c6d5bd96dd055
Henrik Bengtsson <hb@aroma-project.org>
2021-04-21 20:03:24 -0700

    TROUBLESHOOTING: test with trace() in package function [ci skip]

--- R/hello.R
+++ R/hello.R
@@ -1,2 +1,12 @@
-hello <- function() message("Hello world!")
+#' @importFrom utils head
+#' @importFrom tools R_user_dir
+#' @export
+hello <- function() {
+  tracer <- quote(.Platform <- list(OS.type = "windows"))
+  trace(tools::R_user_dir, tracer = tracer)
+  on.exit(untrace(tools::R_user_dir))
+  print(head(deparse(body(tools::R_user_dir@.Data))))
+  tools::R_user_dir("R.cache", which = "cache")
+}
+
 
--- tests/hello.R
+++ tests/hello.R
@@ -2,16 +2,23 @@ library(teeny)
 
 if (.Platform$OS.type != "windows") Sys.setenv(LOCALAPPDATA = "WORKS:")
 
-if (getRversion() >= "4.0.0") {
-  path <- local({
-    tracer <- quote(.Platform <- list(OS.type = "windows"))
-    trace(tools::R_user_dir, tracer = tracer)
-    on.exit(untrace(tools::R_user_dir))
-    print(head(deparse(body(tools::R_user_dir@.Data))))
-    tools::R_user_dir("R.cache", which = "cache")
-  })
-  
-  print(path)
-}
 
+message("trace():")
+
+path <- local({
+  tracer <- quote(.Platform <- list(OS.type = "windows"))
+  trace(tools::R_user_dir, tracer = tracer)
+  on.exit(untrace(tools::R_user_dir))
+  print(head(deparse(body(tools::R_user_dir@.Data))))
+  tools::R_user_dir("R.cache", which = "cache")
+})
+
+print(path)
+
+
+
+
+
+message("hello():")
+print(hello())
 
