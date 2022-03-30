6a484e5754066fe64d4b07d42c721948e8726a34
Henrik Bengtsson <hb@aroma-project.org>
2022-03-30 16:16:49 -0700

    detrius -> detritus

--- tests/detritus.R
+++ tests/detritus.R
@@ -27,12 +27,12 @@ tmp_after <- dir(tmp_root, full.names = FALSE)
 tmp_diff <- setdiff(tmp_after, tmp_before)
 print(tmp_diff)
 
-message("Detrious temporary files:")
+message("Detritus temporary files:")
 res$pid_hex <- sprintf("%x", res$pid)
-res$detrius <- NA_character_
+res$detritus <- NA_character_
 for (kk in 1:nrow(res)) {
   pattern <- sprintf("^Rscript%s", res$pid_hex[kk])
   match <- grep(pattern, basename(tmp_diff), value = TRUE)
-  if (length(match) > 0) res$detrius[kk] <- match
+  if (length(match) > 0) res$detritus[kk] <- match
 }
 print(res)
