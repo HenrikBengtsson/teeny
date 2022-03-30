d37296113528b7603ee927fc41653c11f2acb81b
Henrik Bengtsson <hb@aroma-project.org>
2022-03-30 15:43:46 -0700

    Reporting on detrius files from within the test script

--- tests/detrius.R
+++ tests/detrius.R
@@ -1,3 +1,8 @@
+message("Temporary files in before:")
+tmp_root <- dirname(tempdir())
+tmp_before <- dir(tmp_root, full.names = TRUE)
+print(tmp_before)
+
 cl <- parallel::makeCluster(1L)
 
 res <- parallel::clusterEvalQ(cl, {
@@ -16,3 +21,21 @@ pids_hex <- sapply(pids, FUN = sprintf, fmt = "%x")
 print(pids_hex)
 
 parallel::stopCluster(cl)
+
+message("Temporary files after:")
+tmp_after <- dir(tmp_root, full.names = TRUE)
+print(tmp_after)
+
+message("Added temporary files and folders:")
+tmp_diff <- setdiff(tmp_after, tmp_before)
+print(tmp_diff)
+
+message("Detrious temporary files:")
+detrius <- grep("^Rscript", basename(tmp_diff), value = TRUE)
+print(detrius)
+for (name in names(pids)) {
+  match <- grep(sprintf("^Rscript%x", pids[name]), detrius, value = TRUE)
+  if (length(match) > 0) {
+    message(sprintf("%s left detrius temporary file %s", sQuote(name), paste(match, collapse = ", ")))
+  }
+}
