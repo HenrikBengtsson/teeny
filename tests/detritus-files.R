7c6a21285716e62c5965f32836823fc9814bceee
Henrik Bengtsson <hb@aroma-project.org>
2022-10-03 12:27:56 -0700

    GHA: trying again

---
+++ tests/detritus-files.R
@@ -0,0 +1,13 @@
+## Create two parallel workers
+cl <- parallel::makePSOCKcluster(2L)
+
+## Say hello
+message("Hello world!")
+
+## If we leave the following out, we will get an 'R CMD check' NOTE
+## on detritus in the temp directory, where the files have names of
+## format Rscript<hexcode>, when running on MS Windows (only)
+if (grepl("tests", Sys.getenv("R_PARALLEL_STOPCLUSTER"))) {
+  parallel::stopCluster(cl)
+}
+
