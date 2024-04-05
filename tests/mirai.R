a05070a9b89d5699ae7e7c1279633653eaff4829
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2024-04-04 22:13:02 -0700

    package/mirai

---
+++ tests/mirai.R
@@ -0,0 +1,15 @@
+library(mirai)
+
+message("Reset")
+mirai::daemons(0)
+print(mirai::daemons())
+
+message("Launch two workers")
+mirai::daemons(2)
+print(mirai::daemons())
+
+message("Reset")
+mirai::daemons(0)
+print(mirai::daemons())
+
+
