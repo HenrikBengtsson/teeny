bdf688c02f115c83df664aa6f486ad17a9d28321
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2024-04-08 09:57:34 -0700

    TROUBLESHOOTING: Let's wait longer (30s) to see if the dispatcher shuts itself down

--- tests/mirai3.R
+++ tests/mirai3.R
@@ -5,13 +5,13 @@ mirai::daemons(0)
 print(mirai::daemons())
 
 message("Launch two workers")
-mirai::daemons(2, dispatcher = FALSE)
+mirai::daemons(2)
 print(mirai::daemons())
 
 message("Reset")
 mirai::daemons(0)
 print(mirai::daemons())
 
-Sys.sleep(5)
+Sys.sleep(30)
 
 
