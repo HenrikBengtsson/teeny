c2e3bf93e56b46fc61aafa3652068a921f77f0c9
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2024-04-09 11:39:52 -0700

    Trying to figure out where the stray Rscript<hash> folders orginate from

--- tests/mirai.R
+++ tests/mirai.R
@@ -1,8 +1,20 @@
 library(mirai)
 
 message("Launch two workers")
-mirai::daemons(2, url = local_url(), output = TRUE)
+mirai::daemons(2, output = TRUE)
 print(mirai::daemons())
+
+message("Temporary directory for main process: ", tempdir())
+
+message("Temporary directories for parallel workers:")
+ms <- list()
+for (kk in 1:10) {
+  ms[[kk]] <- mirai({ Sys.sleep(1); tempdir() })
+}
+vs <- vapply(ms, FUN = function(m) call_mirai(m)$data, FUN.VALUE = NA_character_)
+print(table(vs))
+
+
 
 message("Reset")
 mirai::daemons(0)
